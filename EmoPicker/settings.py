7462b3e84060b5452c02baaf12590c4175d9e875
SoulFletcher <not1ikeother@mail.ru>
2023-03-21 16:59:55 +0000

    Changes to be committed: 	modified:   EmoPicker/settings.py 	modified:   Homepage/static/styles.css 	modified:   Homepage/templates/hello.html 	modified:   Homepage/templates/index.html 	modified:   Homepage/views.py 	modified:   db.sqlite3
     Untracked files:
    	EmoPicker/__pycache__/__init__.cpython-38.pyc
    	EmoPicker/__pycache__/settings.cpython-38.pyc
    	EmoPicker/__pycache__/urls.cpython-38.pyc
    	Homepage/__pycache__/__init__.cpython-38.pyc
    	Homepage/__pycache__/admin.cpython-38.pyc
    	Homepage/__pycache__/apps.cpython-38.pyc
    	Homepage/__pycache__/models.cpython-38.pyc
    	Homepage/__pycache__/views.cpython-38.pyc
    	Homepage/static/toggle.js
    	static/

--- EmoPicker/settings.py
+++ EmoPicker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sei(dq%cx9w%vek#r_4dhn^m-tv59-h%txf!)w3hl5k(=ln-o_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['datasetcollect.pythonanywhere.com']
 
 
 # Application definition
@@ -117,7 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
--- Homepage/views.py
+++ Homepage/views.py
@@ -15,23 +15,27 @@ def home(request):
 
 def change(request):
     if request.method == 'GET':
-        choice = int(request.GET['to_change'])
+        choice = int(request.GET.get('to_change', 0))
+        Emo = int(request.GET.get('state[Emo]', 0))
+        Neu = int(request.GET.get('state[Neu]', 0))
+        Obj = int(request.GET.get('state[Obj]', 0))
+        Sub = int(request.GET.get('state[Sub]', 0))
         factor = float(request.GET['factor'])
         index = request.GET['text_id']
         size = DataSetText.objects.count()
         text = DataSetText.objects.get(pk=index)
-        if choice == 1:
+        if Emo == 1:
             text.rating += 1 * factor
-        elif choice == 2:
+        if Neu == 1:
             text.rating -= 1 * factor
-        elif choice == 3:
+        if Obj == 1:
             text.is_estimated += 1 * factor
-        elif choice == 4:
+        if Sub == 1:
             text.is_estimated -= 1 * factor
-        elif choice == 5:
+        if choice == 5:
             text.is_garbage += 1
         text.save()
         b = random.randint(1, size)
         a = DataSetText.objects.get(pk=b)
         return JsonResponse({'text':a.text, 'index':int(b)})
-        
+
